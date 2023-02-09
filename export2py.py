d711c7590090341d3c8412e9cd1a0d4ac80a5bbe
Nishi <nishiyk256@gmail.com>
2023-02-09 17:31:21 +0900

    extend Export addon

--- export2py.py
+++ export2py.py
@@ -1,12 +1,7 @@
 """Export flow to python requests."""
-import logging
 
-import pyperclip
-
-import mitmproxy.types
-from mitmproxy import command
 from mitmproxy import flow
-from mitmproxy.addons.export import cleanup_request, pop_headers
+from mitmproxy.addons.export import formats, cleanup_request, pop_headers
 
 
 def python_script(f: flow.Flow) -> str:
@@ -26,35 +21,4 @@ def python_script(f: flow.Flow) -> str:
     return script
 
 
-class Export2py:
-    @command.command("export2py.file")
-    def file(self, flow: flow.Flow, path: mitmproxy.types.Path) -> None:
-        """
-        Export a flow to path.
-        """
-        script = python_script(flow)
-        try:
-            with open(path, "w") as f:
-                    f.write(script)
-        except OSError as e:
-            logging.error(str(e))
-
-    @command.command("export2py.clip")
-    def clip(self, f: flow.Flow) -> None:
-        """
-        Export a flow to the system clipboard.
-        """
-        try:
-            pyperclip.copy(python_script(f))
-        except pyperclip.PyperclipException as e:
-            logging.error(str(e))
-
-    @command.command("export2py")
-    def export_str(self, f: flow.Flow) -> str:
-        """
-        Export a flow to python.
-        """
-        return python_script(f)
-
-
-addons = [Export2py()]
+formats["python_requests"] = python_script
